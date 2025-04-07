8c7a56e8d48c883180ac6cb4d17791d2a22c72c4
qtx <345548264@qq.com>
2025-04-07 13:22:58 +0800

    序列化

--- cdm/cdm_ButtonScannerDlgProductSet.cpp
+++ cdm/cdm_ButtonScannerDlgProductSet.cpp
@@ -15,51 +15,161 @@ rw::cdm::ButtonScannerDlgProductSet::ButtonScannerDlgProductSet(const rw::oso::O
     }
 	outsideDiameterEnable = outsideDiameterEnableItem->getValueAsBool();
 
-	auto outsideDiameterValueEnableItem = oso::ObjectStoreCoreToItem(assembly.getItem("$variable$outsideDiameterEnable$"));
-	if (!outsideDiameterEnableItem) {
-		throw std::runtime_error("$variable$outsideDiameterEnable is not found");
+	auto outsideDiameterValueItem = oso::ObjectStoreCoreToItem(assembly.getItem("$variable$outsideDiameterValue$"));
+	if (!outsideDiameterValueItem) {
+		throw std::runtime_error("$variable$outsideDiameterValue is not found");
 	}
-	outsideDiameterEnable = outsideDiameterEnableItem->getValueAsBool();
+	outsideDiameterValue = outsideDiameterValueItem->getValueAsDouble();
 
-	auto outsideDiameterEnableItem = oso::ObjectStoreCoreToItem(assembly.getItem("$variable$outsideDiameterEnable$"));
-	if (!outsideDiameterEnableItem) {
-		throw std::runtime_error("$variable$outsideDiameterEnable is not found");
+	auto outsideDiameterDeviationItem = oso::ObjectStoreCoreToItem(assembly.getItem("$variable$outsideDiameterDeviation$"));
+	if (!outsideDiameterDeviationItem) {
+		throw std::runtime_error("$variable$outsideDiameterDeviation is not found");
 	}
-	outsideDiameterEnable = outsideDiameterEnableItem->getValueAsBool();
-	bool outsideDiameterEnable{ false };
-	double outsideDiameterValue{ 0 };
-	double outsideDiameterDeviation{ 0 };
+	outsideDiameterDeviation = outsideDiameterDeviationItem->getValueAsDouble();
+	 
+	auto photographyItem = oso::ObjectStoreCoreToItem(assembly.getItem("$variable$photography$"));
+	if (!photographyItem) {
+		throw std::runtime_error("$variable$photography is not found");
+	}
+	photography = photographyItem->getValueAsDouble();
 
-	double photography{ 0 };
-	double blowTime{ 0 };
+	auto blowTimeItem = oso::ObjectStoreCoreToItem(assembly.getItem("$variable$blowTime$"));
+	if (!blowTimeItem) {
+		throw std::runtime_error("$variable$blowTime is not found");
+	}
+	blowTime = blowTimeItem->getValueAsDouble();
 
-	bool edgeDamageEnable{ false };
-	double edgeDamageSimilarity{ false };
+	auto edgeDamageEnableItem = oso::ObjectStoreCoreToItem(assembly.getItem("$variable$edgeDamageEnable$"));
+	if (!edgeDamageEnableItem) {
+		throw std::runtime_error("$variable$edgeDamageEnable is not found");
+	}
+	edgeDamageEnable = edgeDamageEnableItem->getValueAsBool();
 
-	bool shieldingRangeEnable{ false };
-	double outerRadius{ 0 };
-	double innerRadius{ 0 };
+	auto edgeDamageSimilarityItem = oso::ObjectStoreCoreToItem(assembly.getItem("$variable$edgeDamageSimilarity$"));
+	if (!edgeDamageSimilarityItem) {
+		throw std::runtime_error("$variable$edgeDamageSimilarity is not found");
+	}
+	edgeDamageSimilarity = edgeDamageSimilarityItem->getValueAsBool();
 
-	bool poreEnable{ false };
-	bool paintEnable{ false };
+	auto shieldingRangeEnableItem = oso::ObjectStoreCoreToItem(assembly.getItem("$variable$shieldingRangeEnable$"));
+	if (!shieldingRangeEnableItem) {
+		throw std::runtime_error("$variable$shieldingRangeEnable is not found");
+	}
+	shieldingRangeEnable = shieldingRangeEnableItem->getValueAsBool();
 
-	bool holesCountEnable{ false };
-	double holesCountValue{ 0 };
+	auto outerRadiusItem = oso::ObjectStoreCoreToItem(assembly.getItem("$variable$outerRadius$"));
+	if (!outerRadiusItem) {
+		throw std::runtime_error("$variable$outerRadius is not found");
+	}
+	outerRadius = outerRadiusItem->getValueAsDouble();
 
-	bool brokenEyeEnable{ false };
-	double brokenEyeSimilarity{ 0 };
+	auto innerRadiusItem = oso::ObjectStoreCoreToItem(assembly.getItem("$variable$innerRadius$"));
+	if (!innerRadiusItem) {
+		throw std::runtime_error("$variable$innerRadius is not found");
+	}
+	innerRadius = innerRadiusItem->getValueAsDouble();
+ 
+	auto poreEnableItem = oso::ObjectStoreCoreToItem(assembly.getItem("$variable$poreEnable$"));
+	if (!poreEnableItem) {
+		throw std::runtime_error("$variable$poreEnable is not found");
+	}
+	poreEnable = poreEnableItem->getValueAsBool();
 
-	bool crackEnable{ false };
-	double crackSimilarity{ 0 };
+	auto paintEnableItem = oso::ObjectStoreCoreToItem(assembly.getItem("$variable$paintEnable$"));
+	if (!paintEnableItem) {
+		throw std::runtime_error("$variable$paintEnable is not found");
+	}
+	paintEnable = paintEnableItem->getValueAsBool();
 
-	bool apertureEnable{ false };
-	double apertureValue{ 0 };
-	double apertureSimilarity{ 0 };
+	auto holesCountEnableItem = oso::ObjectStoreCoreToItem(assembly.getItem("$variable$holesCountEnable$"));
+	if (!holesCountEnableItem) {
+		throw std::runtime_error("$variable$holesCountEnable is not found");
+	}
+	holesCountEnable = holesCountEnableItem->getValueAsBool();
+	 
+	auto holesCountValueItem = oso::ObjectStoreCoreToItem(assembly.getItem("$variable$holesCountValue$"));
+	if (!holesCountValueItem) {
+		throw std::runtime_error("$variable$holesCountValue is not found");
+	}
+	holesCountValue = holesCountValueItem->getValueAsDouble();
 
-	bool holeCenterDistanceEnable{ false };
-	double holeCenterDistanceValue{ 0 };
-	double holeCenterDistanceSimilarity{ 0 };
+	auto brokenEyeEnableItem = oso::ObjectStoreCoreToItem(assembly.getItem("$variable$brokenEyeEnable$"));
+	if (!brokenEyeEnableItem) {
+		throw std::runtime_error("$variable$brokenEyeEnable is not found");
+	}
+	brokenEyeEnable = brokenEyeEnableItem->getValueAsBool();
 
+	auto brokenEyeSimilarityItem = oso::ObjectStoreCoreToItem(assembly.getItem("$variable$brokenEyeSimilarity$"));
+	if (!brokenEyeSimilarityItem) {
+		throw std::runtime_error("$variable$brokenEyeSimilarity is not found");
+	}
+	brokenEyeSimilarity = brokenEyeSimilarityItem->getValueAsDouble();
+
+	auto crackSimilarityItem = oso::ObjectStoreCoreToItem(assembly.getItem("$variable$crackSimilarity$"));
+	if (!crackSimilarityItem) {
+		throw std::runtime_error("$variable$crackSimilarity is not found");
+	}
+	crackSimilarity = crackSimilarityItem->getValueAsDouble();
+
+	auto crackEnableItem = oso::ObjectStoreCoreToItem(assembly.getItem("$variable$crackEnable$"));
+	if (!crackEnableItem) {
+		throw std::runtime_error("$variable$crackEnable is not found");
+	}
+	crackEnable = crackEnableItem->getValueAsBool();
+
+	auto apertureEnableItem = oso::ObjectStoreCoreToItem(assembly.getItem("$variable$apertureEnable$"));
+	if (!apertureEnableItem) {
+		throw std::runtime_error("$variable$apertureEnable is not found");
+	}
+	apertureEnable = apertureEnableItem->getValueAsBool();
+
+	auto apertureValueItem = oso::ObjectStoreCoreToItem(assembly.getItem("$variable$apertureValue$"));
+	if (!apertureValueItem) {
+		throw std::runtime_error("$variable$apertureValue is not found");
+	}
+	apertureValue = apertureValueItem->getValueAsDouble();
+
+	auto apertureSimilarityItem = oso::ObjectStoreCoreToItem(assembly.getItem("$variable$apertureSimilarity$"));
+	if (!apertureSimilarityItem) {
+		throw std::runtime_error("$variable$apertureSimilarity is not found");
+	}
+	apertureSimilarity = apertureSimilarityItem->getValueAsDouble();
+	 
+	auto holeCenterDistanceEnableItem = oso::ObjectStoreCoreToItem(assembly.getItem("$variable$holeCenterDistanceEnable$"));
+	if (!holeCenterDistanceEnableItem) {
+		throw std::runtime_error("$variable$holeCenterDistanceEnable is not found");
+	}
+	holeCenterDistanceEnable = holeCenterDistanceEnableItem->getValueAsBool();
+
+	auto holeCenterDistanceValueItem = oso::ObjectStoreCoreToItem(assembly.getItem("$variable$holeCenterDistanceValue$"));
+	if (!holeCenterDistanceValueItem) {
+		throw std::runtime_error("$variable$holeCenterDistanceValue is not found");
+	}
+	holeCenterDistanceValue = holeCenterDistanceValueItem->getValueAsDouble();
+
+	auto holeCenterDistanceSimilarityItem = oso::ObjectStoreCoreToItem(assembly.getItem("$variable$holeCenterDistanceSimilarity$"));
+	if (!holeCenterDistanceSimilarityItem) {
+		throw std::runtime_error("$variable$holeCenterDistanceSimilarity is not found");
+	}
+	holeCenterDistanceSimilarity = holeCenterDistanceSimilarityItem->getValueAsDouble();
+ 
+	auto holeCenterDistanceSimilarityItem = oso::ObjectStoreCoreToItem(assembly.getItem("$variable$holeCenterDistanceSimilarity$"));
+	if (!holeCenterDistanceSimilarityItem) {
+		throw std::runtime_error("$variable$holeCenterDistanceSimilarity is not found");
+	}
+	holeCenterDistanceSimilarity = holeCenterDistanceSimilarityItem->getValueAsDouble();
+
+	auto holeCenterDistanceSimilarityItem = oso::ObjectStoreCoreToItem(assembly.getItem("$variable$holeCenterDistanceSimilarity$"));
+	if (!holeCenterDistanceSimilarityItem) {
+		throw std::runtime_error("$variable$holeCenterDistanceSimilarity is not found");
+	}
+	holeCenterDistanceSimilarity = holeCenterDistanceSimilarityItem->getValueAsDouble();
+
+	auto holeCenterDistanceSimilarityItem = oso::ObjectStoreCoreToItem(assembly.getItem("$variable$holeCenterDistanceSimilarity$"));
+	if (!holeCenterDistanceSimilarityItem) {
+		throw std::runtime_error("$variable$holeCenterDistanceSimilarity is not found");
+	}
+	holeCenterDistanceSimilarity = holeCenterDistanceSimilarityItem->getValueAsDouble();
 	bool specifyColorDifferenceEnable{ false };
 	double specifyColorDifferenceR{ 0 };
 	double specifyColorDifferenceG{ 0 };
