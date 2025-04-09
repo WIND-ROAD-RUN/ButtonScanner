6243a1be41ad8193cf144ed7baf51b0708883e49
风 路 <3461571786@qq.com>
2025-04-09 13:55:19 +0800

    添加曝光设置的防呆机制，保证数值在0~700

--- ButtonScanner/DlgExposureTimeSet.cpp
+++ ButtonScanner/DlgExposureTimeSet.cpp
@@ -2,6 +2,7 @@
 #include "DlgExposureTimeSet.h"
 
 #include"NumKeyBord.h"
+#include"GlobalStruct.h"
 
 DlgExposureTimeSet::DlgExposureTimeSet(QWidget *parent)
     : QDialog(parent)
@@ -19,6 +20,9 @@ DlgExposureTimeSet::~DlgExposureTimeSet()
 
 void DlgExposureTimeSet::build_ui()
 {
+    auto& globalStruct = GlobalStruct::getInstance();
+    auto& exposureTimeSetConfig = globalStruct.dlgExposureTimeSetConfig;
+    ui->pbtn_exposureTimeValue->setText(QString::number(exposureTimeSetConfig.expousureTime));
 }
 
 void DlgExposureTimeSet::build_connect()
@@ -29,8 +33,29 @@ void DlgExposureTimeSet::build_connect()
 
 void DlgExposureTimeSet::pbtn_exposureTimeValue_clicked()
 {
+    auto olderValue=ui->pbtn_exposureTimeValue->text().toInt();
     auto numKeyBord = new NumKeyBord(this,ui->pbtn_exposureTimeValue,1);
     numKeyBord->exec();
     delete numKeyBord;
+
+    auto newValue = ui->pbtn_exposureTimeValue->text().toInt();
+    if (newValue<=0) {
+        ui->pbtn_exposureTimeValue->setText(QString::number(olderValue));
+        QMessageBox::warning(this, "错误", "曝光时间范围应设置为0~700");
+        return;
+    }
+    if (newValue>700) {
+        ui->pbtn_exposureTimeValue->setText(QString::number(olderValue));
+        QMessageBox::warning(this, "错误", "曝光时间范围应设置为0~700");
+        return;
+    }
+    if (newValue == olderValue) {
+        return;
+    }
+
+    auto& globalStruct = GlobalStruct::getInstance();
+    globalStruct.dlgExposureTimeSetConfig.expousureTime = newValue;
+
+    globalStruct.saveDlgExposureTimeSetConfig();
 
 }
