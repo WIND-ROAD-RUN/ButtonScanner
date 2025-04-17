c46368ec04b9ad4cab9626d54da1a787113bfe3f
RoadWind <3461571786@qq.com>
2025-04-17 13:47:46 +0800

    完成新物料窗口的空间名字修改

--- ButtonScanner/DlgNewProduction.cpp
+++ ButtonScanner/DlgNewProduction.cpp
@@ -17,9 +17,170 @@ DlgNewProduction::~DlgNewProduction()
 
 void DlgNewProduction::build_ui()
 {
-	//ui->tabWidget->tabBar()->hide();
+	ui->tabWidget->tabBar()->hide();
+	build_dialog();
 }
 
 void DlgNewProduction::build_connect()
 {
+	//tab1
+	QObject::connect(ui->pbtn_tab1_exit, &QPushButton::clicked,
+		this, &DlgNewProduction::pbtn_tab1_exit_clicked);
+	QObject::connect(ui->pbtn_tab1_no, &QPushButton::clicked,
+		this, &DlgNewProduction::pbtn_tab1_no_clicked);
+	QObject::connect(ui->pbtn_tab1_ok, &QPushButton::clicked,
+		this, &DlgNewProduction::pbtn_tab1_ok_clicked);
+	//tab2
+	QObject::connect(ui->pbtn_tab2_checkColor, &QPushButton::clicked,
+		this, &DlgNewProduction::pbtn_tab2_checkColor_clicked);
+	QObject::connect(ui->pbtn_tab2_checkBladeShape, &QPushButton::clicked,
+		this, &DlgNewProduction::pbtn_tab2_checkBladeShape_clicked);
+	QObject::connect(ui->pbtn_tab2_preStep, &QPushButton::clicked,
+		this, &DlgNewProduction::pbtn_tab2_preStep_clicked);
+	QObject::connect(ui->pbtn_tab2_exit, &QPushButton::clicked,
+		this, &DlgNewProduction::pbtn_tab2_exit_clicked);
+	//tab3
+	QObject::connect(ui->pbtn_tab3_openImgLocate, &QPushButton::clicked,
+		this, &DlgNewProduction::pbtn_tab3_openImgLocate_clicked);
+	QObject::connect(ui->pbtn_tab3_exit, &QPushButton::clicked,
+		this, &DlgNewProduction::pbtn_tab3_exit_clicked);
+	QObject::connect(ui->pbtn_tab3_preStep, &QPushButton::clicked,
+		this, &DlgNewProduction::pbtn_tab3_preStep_clicked);
+	QObject::connect(ui->pbtn_tab3_nexStep, &QPushButton::clicked,
+		this, &DlgNewProduction::pbtn_tab3_nexStep_clicked);
+	//tab4
+	QObject::connect(ui->pbtn_tab4_openImgLocate, &QPushButton::clicked,
+		this, &DlgNewProduction::pbtn_tab4_openImgLocate_clicked);
+	QObject::connect(ui->pbtn_tab4_exit, &QPushButton::clicked,
+		this, &DlgNewProduction::pbtn_tab4_exit_clicked);
+	QObject::connect(ui->pbtn_tab4_preStep, &QPushButton::clicked,
+		this, &DlgNewProduction::pbtn_tab4_preStep_clicked);
+	QObject::connect(ui->pbtn_tab4_nexStep, &QPushButton::clicked,
+		this, &DlgNewProduction::pbtn_tab4_nexStep_clicked);
+	//tab5
+	QObject::connect(ui->pbtn_tab5_startTrain, &QPushButton::clicked,
+		this, &DlgNewProduction::pbtn_tab5_startTrain_clicked);
+	QObject::connect(ui->pbtn_tab5_exit, &QPushButton::clicked,
+		this, &DlgNewProduction::pbtn_tab5_exit_clicked);
+	QObject::connect(ui->pbtn_tab5_preStep, &QPushButton::clicked,
+		this, &DlgNewProduction::pbtn_tab5_preStep_clicked);
+	QObject::connect(ui->pbtn_tab5_finish, &QPushButton::clicked,
+		this, &DlgNewProduction::pbtn_tab5_finish_clicked);
+}
+
+void DlgNewProduction::build_dialog()
+{
+	ui->tabWidget->setCurrentIndex(0);
+}
+
+void DlgNewProduction::destroy()
+{
+	_info.state = DlgNewProductionInfo::None;
+	ui->tabWidget->setCurrentIndex(0);
+	this->hide();
+}
+
+void DlgNewProduction::pbtn_tab1_no_clicked()
+{
+	ui->tabWidget->setCurrentIndex(1);
+}
+
+void DlgNewProduction::pbtn_tab1_exit_clicked()
+{
+	destroy();
+}
+
+void DlgNewProduction::pbtn_tab2_checkColor_clicked()
+{
+	ui->rbtn_tab3_filterColor->setChecked(true);
+	ui->rbtn_tab4_filterColor->setChecked(true);
+	ui->rbtn_tab5_filterColor->setChecked(true);
+	this->_info.state = _info.state = DlgNewProductionInfo::CheckColor;
+	ui->tabWidget->setCurrentIndex(2);
+}
+
+void DlgNewProduction::pbtn_tab2_checkBladeShape_clicked()
+{
+	ui->rbtn_tab3_filterColor->setChecked(false);
+	ui->rbtn_tab4_filterColor->setChecked(false);
+	ui->rbtn_tab5_filterColor->setChecked(false);
+	this->_info.state = _info.state = DlgNewProductionInfo::CheckBladeShape;
+	ui->tabWidget->setCurrentIndex(2);
+}
+
+void DlgNewProduction::pbtn_tab2_preStep_clicked()
+{
+	ui->tabWidget->setCurrentIndex(0);
+}
+
+void DlgNewProduction::pbtn_tab2_exit_clicked()
+{
+	destroy();
+
+}
+
+void DlgNewProduction::pbtn_tab3_openImgLocate_clicked()
+{
+}
+
+void DlgNewProduction::pbtn_tab3_exit_clicked()
+{
+	destroy();
+}
+
+void DlgNewProduction::pbtn_tab3_preStep_clicked()
+{
+	ui->tabWidget->setCurrentIndex(1);
+	this->_info.state = _info.state = DlgNewProductionInfo::None;
+	ui->rbtn_tab3_filterColor->setChecked(false);
+	ui->rbtn_tab4_filterColor->setChecked(false);
+	ui->rbtn_tab5_filterColor->setChecked(false);
+}
+
+void DlgNewProduction::pbtn_tab3_nexStep_clicked()
+{
+	ui->tabWidget->setCurrentIndex(3);
+}
+
+void DlgNewProduction::pbtn_tab4_openImgLocate_clicked()
+{
+}
+
+void DlgNewProduction::pbtn_tab4_exit_clicked()
+{
+	destroy();
+}
+
+void DlgNewProduction::pbtn_tab4_preStep_clicked()
+{
+	ui->tabWidget->setCurrentIndex(2);
+}
+
+void DlgNewProduction::pbtn_tab4_nexStep_clicked()
+{
+	ui->tabWidget->setCurrentIndex(4);
+}
+
+void DlgNewProduction::pbtn_tab5_startTrain_clicked()
+{
+}
+
+void DlgNewProduction::pbtn_tab5_exit_clicked()
+{
+	destroy();
+}	
+
+void DlgNewProduction::pbtn_tab5_preStep_clicked()
+{
+	ui->tabWidget->setCurrentIndex(3);
+}
+
+void DlgNewProduction::pbtn_tab5_finish_clicked()
+{
+	destroy();
+}
+
+void DlgNewProduction::pbtn_tab1_ok_clicked()
+{
+	ui->tabWidget->setCurrentIndex(1);
 }
--- ButtonScanner/DlgNewProduction.h
+++ ButtonScanner/DlgNewProduction.h
@@ -7,6 +7,17 @@ QT_BEGIN_NAMESPACE
 namespace Ui { class DlgNewProductionClass; };
 QT_END_NAMESPACE
 
+
+struct DlgNewProductionInfo
+{
+	enum State
+	{
+		None,
+		CheckBladeShape,
+		CheckColor
+	}state;
+};
+
 class DlgNewProduction : public QDialog
 {
 	Q_OBJECT
@@ -20,4 +31,35 @@ private:
 
 private:
 	Ui::DlgNewProductionClass *ui;
+
+private:
+	void build_dialog();
+	void destroy();
+
+private:
+	DlgNewProductionInfo _info;
+private slots:
+	void pbtn_tab1_ok_clicked();
+	void pbtn_tab1_no_clicked();
+	void pbtn_tab1_exit_clicked();
+private slots:
+	void pbtn_tab2_checkColor_clicked();
+	void pbtn_tab2_checkBladeShape_clicked();
+	void pbtn_tab2_preStep_clicked();
+	void pbtn_tab2_exit_clicked();
+private slots:
+	void pbtn_tab3_openImgLocate_clicked();
+	void pbtn_tab3_exit_clicked();
+	void pbtn_tab3_preStep_clicked();
+	void pbtn_tab3_nexStep_clicked();
+private slots:
+	void pbtn_tab4_openImgLocate_clicked();
+	void pbtn_tab4_exit_clicked();
+	void pbtn_tab4_preStep_clicked();
+	void pbtn_tab4_nexStep_clicked();
+private slots:
+	void pbtn_tab5_startTrain_clicked();
+	void pbtn_tab5_exit_clicked();
+	void pbtn_tab5_preStep_clicked();
+	void pbtn_tab5_finish_clicked();
 };
